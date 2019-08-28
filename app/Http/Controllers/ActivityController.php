9c027b42d8cfc027345cab9efa1fefc68dd4d7c6
leadstar116 <supremedev116@gmail.com>
2019-08-29 02:35:25 +1000

    activity done

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use App\Users;
 
 class ActivityController extends Controller
 {
@@ -25,6 +26,46 @@ class ActivityController extends Controller
      */
     public function index()
     {
-        return view('activity')->with('user', Auth::user());
+        $user = Auth::user();
+        $result = DB::table('tips')
+            ->where('tiIsActive', '=', '1')
+            ->orderBy('iCreatedAt', 'desc')->get();
+        $activities = [];
+        foreach($result as $item) {
+            $print_rate = self::print_stars(self::round_half($item->vRating));
+
+            $employ = Users::where('iUserId', $item->iToUserId)->get();
+            $guest = Users::where('iUserId', $item->iFromUserId)->get();
+
+            $activities[] = [
+                'activity' => $item,
+                'print_rating' => $print_rate,
+                'employee_name' => $employ[0]->vFirstName. ' ' .$employ[0]->vLastName,
+                'employee_email' => $employ[0]->vEmailId,
+                'guest_name' => $guest[0]->vFirstName. ' ' .$guest[0]->vLastName,
+                'guest_email' => $guest[0]->vEmailId,
+            ];
+        }
+
+        return view('activity')->with('user', Auth::user())->with('activities', $activities);
+    }
+
+    public function round_half($num) {
+        return round($num * 2) / 2;
+    }
+
+    public function print_stars($num) {
+        $stars = '<ul class="review-rating star-rating">';
+        for ($n=0; $n<=4; $n++) {
+            $stars .= '<li><span class="fa fa-1x fa-star';
+            if ($num==$n+.5) {
+                $stars .= '-half-empty';
+            } elseif ($num<$n+.5) {
+                $stars .= '-o';
+            };
+            $stars .= '"></span></li>';
+        };
+        $stars .= '</ul>';
+        return $stars;
     }
 }
