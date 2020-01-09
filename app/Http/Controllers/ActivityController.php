07c734c457846582ba9b3773036341be22870ad2
leadstar116 <supremedev116@gmail.com>
2020-01-10 07:36:29 +1000

    activity error fix

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Users;
+use App\BusinessUser;
 
 class ActivityController extends Controller
 {
