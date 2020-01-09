2b0bd8595c1f3c8d04b8ec93b3e840dcadf1e4a8
leadstar116 <supremedev116@gmail.com>
2020-01-10 07:35:30 +1000

    activity error fix

--- app/Http/Controllers/ActivityController.php
+++ app/Http/Controllers/ActivityController.php
@@ -27,25 +27,46 @@ class ActivityController extends Controller
     public function index()
     {
         $user = Auth::user();
+        $activities = [];
+        $user_ids = [];
+        $user_ids[] = $user->id;
+        foreach($user->employees as $employee) {
+            $user_ids[] = $employee->iUserId;
+        }
         $result = DB::table('tips')
+            ->whereIn('iToUserId', $user_ids)
             ->where('tiIsActive', '=', '1')
             ->orderBy('iCreatedAt', 'desc')->get();
-        $activities = [];
         foreach($result as $item) {
             $print_rate = self::print_stars(self::round_half($item->vRating));
 
-            $employ = Users::where('iUserId', $item->iToUserId)->get();
+            if($item->payment_type == 'tips') {
+                $employ = Users::where('iUserId', $item->iToUserId)->get()->toArray();
+            } else {
+                $employ = BusinessUser::where('id', $item->iToUserId)->get()->toArray();
+            }
             $guest = Users::where('iUserId', $item->iFromUserId)->get();
 
             if(!empty($employ) && !empty($guest)) {
-                $activities[] = [
-                    'activity' => $item,
-                    'print_rating' => $print_rate,
-                    'employee_name' => $employ[0]->vFirstName. ' ' .$employ[0]->vLastName,
-                    'employee_email' => $employ[0]->vEmailId,
-                    'guest_name' => $guest[0]->vFirstName. ' ' .$guest[0]->vLastName,
-                    'guest_email' => $guest[0]->vEmailId,
-                ];
+                if($item->payment_type == 'payment') {
+                    $activities[] = [
+                        'activity' => $item,
+                        'print_rating' => $print_rate,
+                        'employee_name' => $employ[0]['FirstName']. ' ' .$employ[0]['LastName'],
+                        'employee_email' => $employ[0]['email'],
+                        'guest_name' => $guest[0]['vFirstName']. ' ' .$guest[0]['vLastName'],
+                        'guest_email' => $guest[0]['vEmailId'],
+                    ];
+                } else {
+                    $activities[] = [
+                        'activity' => $item,
+                        'print_rating' => $print_rate,
+                        'employee_name' => $employ[0]['vFirstName']. ' ' .$employ[0]['vLastName'],
+                        'employee_email' => $employ[0]['vEmailId'],
+                        'guest_name' => $guest[0]['vFirstName']. ' ' .$guest[0]['vLastName'],
+                        'guest_email' => $guest[0]['vEmailId'],
+                    ];
+                }
             }
         }
 
